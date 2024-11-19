822fb0b33f85b9e18a4ffe3a86cea69017aa7133
ramiro999 <ramiro2191920@correo.uis.edu.co>
2024-11-19 11:09:11 -0500

    wip

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 # Script para ejecutar el contenedor y correr GH-ICP
 
 # Montar los directorios necesarios y ejecutar el contenedor
-podman run -it --rm \
+docker run -it --rm \
     -v $(pwd)/cloudpoints:/gh_icp/GH-ICP/samples:rw \
     -e DISPLAY \
     --security-opt label=type:container_runtime_t \
--- shell.sh
+++ shell.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Script para abrir una terminal interactiva dentro del contenedor
 
-podman run -it --rm \
+docker run -it --rm \
     -v $(pwd)/cloudpoints:/gh_icp/GH-ICP/samples:rw \
     -e DISPLAY \
     --security-opt label=type:container_runtime_t \
